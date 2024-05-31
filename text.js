7833515d4a235636e5886092111c3a5c8443c23f
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2024-05-31 21:43:37 +0530

    Update text.js

--- text.js
+++ text.js
@@ -14,15 +14,15 @@ const message4 = document.getElementById('message4');
 
 
 message1.textContent = "Power On";
-message2.textContent = "Hi' Sangeeth ";
-message3.textContent = "I'm Your New Phone";
+message2.textContent = "Hi' Tilan ";
+message3.textContent = "This is Your New Phone";
 message4.textContent = "I have to Say You Somthing";
 
 
 
 
 
-let SecondScreenText1 = [ "Okay Sangeeth"];
+let SecondScreenText1 = [ "Okay Eranga"];
 let SecondScreenText2 = ["Do you know"];
 let SecondScreenText3 = ["is a Good Friend "];
 let SecondScreenText4 = ["So, I'm sure"];
